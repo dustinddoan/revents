7eb7a22b7d1622e6a7e51c9fc4c589ca2490f448
Dustin Doan <dustin.ddoan@gmail.com>
2018-09-26 09:05:01 -0700

    react router

---
+++ src/features/user/PeopleDashboard/PeopleDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PeopleDashboard = () => {
+  return (
+    <div>
+      <h1>people dasdboard
+      </h1>
+    </div>
+  );
+};
+
+export default PeopleDashboard;
